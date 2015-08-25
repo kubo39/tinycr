4c82c81ea04f69c43858d9bf6ced1ca5b04b1562
kubo39 <kubo39@gmail.com>
2015-08-26 02:31:11 +0900

    Add exit syscall and entrypoint

--- hello.cr
+++ hello.cr
@@ -1,4 +1,5 @@
 WRITE = 1_u64
+EXIT = 60_u64
 
 def syscall(n, arg1, arg2, arg3 : UInt64) : UInt64
   dst :: UInt64
@@ -9,5 +10,17 @@ def syscall(n, arg1, arg2, arg3 : UInt64) : UInt64
   return dst
 end
 
-b = "Hello!\n".bytes
-syscall(WRITE, 1_u64, b.buffer.address, b.size.to_u64)
+def syscall(n, arg1 : UInt64) : UInt64
+  dst :: UInt64
+  asm("syscall" : "={rax}"(dst)
+      : "{rax}"(n), "{rdi}"(arg1)
+      : "rcx", "r11", "memory"
+      : "volatile")
+  return dst
+end
+
+def __main
+  b = "Hello!\n".bytes
+  syscall(WRITE, 1_u64, b.buffer.address, b.size.to_u64)
+  syscall(EXIT, 0_u64)
+end
