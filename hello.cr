108139a30dba358890f1056a834610c9af3922e9
kubo39 <kubo39@gmail.com>
2015-08-29 16:02:48 +0900

    Refactoring

--- hello.cr
+++ hello.cr
@@ -13,20 +13,27 @@ class String
   end
 end
 
-def __main
-  write = 1_u64
-  exit = 60_u64
+def syswrite
   dst :: UInt64
+  write = 1_u64
   b = "Hello!\n".to_unsafe
-
   asm("syscall" : "={rax}"(dst)
       : "{rax}"(write), "{rdi}"(1), "{rsi}"(b), "{rdx}"(7)
       : "rcx", "r11", "memory"
       : "volatile")
+end
+
+def sysexit
+  dst :: UInt64
+  exit = 60_u64
   asm("syscall" : "={rax}"(dst)
       : "{rax}"(exit), "{rdi}"(0)
       : "rcx", "r11", "memory"
       : "volatile")
+end
+
+def __main
+  syswrite; sysexit
 end
 
 __main
