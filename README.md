edac9a727fc6cf227a3551eaa7ecb0d30b997cb0
kubo39 <kubo39@gmail.com>
2015-08-29 15:22:02 +0900

    Fix build.sh

--- README.md
+++ README.md
@@ -4,9 +4,6 @@
 $ ./build.sh
 Crystal 0.7.6 [eb13f75] (Thu Aug 13 21:39:15 UTC 2015)
 
-+ '[' -e ivmai ']'
-+ export LIBRARY_PATH=./bdwgc/.libs
-+ LIBRARY_PATH=./bdwgc/.libs
 + crystal build hello.cr --emit obj --prelude=empty --release
 + gcc hello.o -o hello -s
 + wc -c
--- build.sh
+++ build.sh
@@ -9,19 +9,6 @@ echo
 
 set -x
 
-if [ -e $dirname ]; then
-  git clone git://github.com/ivmai/libatomic_ops.git
-  git clone git://github.com/ivmai/bdwgc.git
-  ln -s `pwd`/libatomic_ops `pwd`/bdwgc/libatomic_ops
-  cd bdwgc
-  autoreconf -vif
-  automake --add-missing
-  ./configure
-  make
-  cd ..
-fi
-
-export LIBRARY_PATH=./bdwgc/.libs
 crystal build hello.cr --emit obj --prelude=empty --release
 gcc hello.o -o hello -s
 wc -c < hello
