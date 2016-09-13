366f2f1f1f18bc03b1c3e9c2dde3ba7ce3f89f0d
Hiroki Noda <kubo39@gmail.com>
2016-09-13 09:04:20 +0900

    Remove function.

--- hello.cr
+++ hello.cr
@@ -1,23 +1,11 @@
-def syswrite
-  dst = 0_u64
-  write = 1_u64
-  asm("syscall" : "={rax}"(dst)
-      : "{rax}"(write), "{rdi}"(1), "{rsi}"(0x400008), "{rdx}"(7)
-      : "rcx", "r11", "memory"
-      : "volatile")
-end
-
-def sysexit
-  dst = 0_u64
-  exit = 60_u64
-  asm("syscall" : "={rax}"(dst)
-      : "{rax}"(exit), "{rdi}"(0)
-      : "rcx", "r11", "memory"
-      : "volatile")
-end
-
-def __main
-  syswrite; sysexit
-end
-
-__main
+dst = 0_u64
+write = 1_u64
+exit = 60_u64
+asm("syscall" : "={rax}"(dst)
+    : "{rax}"(write), "{rdi}"(1), "{rsi}"(0x400008), "{rdx}"(7)
+    : "rcx", "r11", "memory"
+    : "volatile")
+asm("syscall" : "={rax}"(dst)
+    : "{rax}"(60), "{rdi}"(0)
+    : "rcx", "r11", "memory"
+    : "volatile")
