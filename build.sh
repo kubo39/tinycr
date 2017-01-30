fa30e8c925ef51171395203fd320b13fe624d393
Hiroki Noda <kubo39@gmail.com>
2017-01-30 22:11:19 +0900

    Up

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
+export LANG=C
 
 crystal -v
 echo
@@ -8,9 +9,10 @@ echo
 set -x
 
 crystal build hello.cr --emit obj --prelude=empty --release --verbose
+objdump -dr hello.o
 ld hello.o -o hello --static -nostdlib --gc-sections -T script.ld
 objcopy -j combined -O binary hello hello.bin
-ENTRY=$(nm -f posix hello | grep 'main' | awk '{print $3}')
+ENTRY=$(nm --format=posix hello | grep 'main' | awk '{print $3}')
 nasm -f bin -o tinybin -D entry=0x$ENTRY elf.s
 chmod +x tinybin
 hexdump -C tinybin
